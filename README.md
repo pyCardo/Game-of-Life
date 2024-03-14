b4234d982827c32948f833a5c35f19869502ceda
Nicolò Cardinali <nicolo.cardinali@gmail.com>
2024-03-14 19:45:21 +0100

    SFML now working
    Testing of the Grid::update function required

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-Implementazione di Game of Life in C++.
+# Game of Life in C++
 Risorse del progetto:
-Wikipedia - Conway's Game of Life: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life.
+- Wikipedia [_Conway's Game of Life_](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life).
+~~~bash
+$ g++ gameOfLife.cpp -Wall -Wextra -lsfml-graphics -lsfml-window -lsfml-system
+$ export DISPLAY=:0
+$ export DISPLAY="$(/sbin/ip route | awk '/default/ { print $3 }'):0"
+$ export LIBGL_ALWAYS_INDIRECT=0
+~~~
+Execute inside GUI (MobaXterm).
+NOTE: add time and population counters.
--- gameOfLife.cpp
+++ gameOfLife.cpp
@@ -7,6 +7,9 @@
 // #include <iostream>
 // #include <string>
 
+#include "doctest.h"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 class Cell {
   bool isAlive_{false};
 
@@ -20,10 +23,14 @@ bool Cell::update(std::vector<Cell> data, int xIndex, int yIndex, int rowSize) {
   int neighbours{};
   for (int x{xIndex - 1}; x <= xIndex + 1; x++) {
     for (int y{yIndex - 1}; y <= yIndex + 1; y++) {
+      if (x < 0 || y < 0 || x >= rowSize ||
+          y >= (static_cast<int>(data.size()) / rowSize))
+        continue;  // skip the elements outside of the border
       if (x == xIndex && y == yIndex) continue;
       if (data[xIndex + yIndex * rowSize].isAlive_) neighbours++;
     }
   }
+  assert(neighbours >= 0 && neighbours <= 2);  // this assert is failing
 
   if (isAlive_) {
     if (neighbours == 2 || neighbours == 3) return true;
@@ -35,14 +42,14 @@ bool Cell::update(std::vector<Cell> data, int xIndex, int yIndex, int rowSize) {
 
 class Grid {
   sf::Image image_;
+  int width_;
+  int height_;
   int cellSize_;
   int rows_;
   int columns_;
-  int width_;
-  int height_;
-  std::vector<Cell> data;
 
  public:
+  std::vector<Cell> data;
   Grid(int w, int h, int cs) : width_{w}, height_{h}, cellSize_{cs} {
     assert(w > 0 && h > 0 && cellSize_ > 0);
     assert(w % cellSize_ == 0 && h % cellSize_ == 0);
@@ -69,33 +76,36 @@ void Grid::draw(int c, int r) {
 
 void Grid::play() {
   sf::RenderWindow window(sf::VideoMode(width_, height_), "Game of Life");
-  assert(1==0);
-  window.setFramerateLimit(60);
+  window.setFramerateLimit(3);
   sf::Texture texture;
-  texture.loadFromImage(image_);
   sf::Sprite sprite;
-  sprite.setTexture(texture);
+
+  texture.loadFromImage(image_);
+
   while (window.isOpen()) {
     sf::Event event;
+
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed) {
         window.close();
       }
     }
 
-    for (int i{0}; i < data.size(); i++) {
-      int x{i / columns_};
-      int y{i % columns_};
-      assert(x + y * columns_ <= i);
+    for (int i{0}; i < static_cast<int>(data.size()); i++) {
+      int x{i % columns_};
+      int y{i / columns_};
+      assert(x + y * columns_ == i);
       data[i].nextAlive = data[i].update(data, x, y, columns_);
+      assert(data[i].nextAlive == false);
       if (data[i].nextAlive) draw(x, y);
     }
 
-    for (int i{0}; i < data.size(); i++) {
+    for (int i{0}; i < static_cast<int>(data.size()); i++) {
       data[i].setAlive(data[i].nextAlive);
     }
 
     texture.update(image_);
+    sprite.setTexture(texture);
 
     window.clear();
     window.draw(sprite);
@@ -103,12 +113,29 @@ void Grid::play() {
   }
 }
 
-int main() {
-  int const width{600};
-  int const height{600};
-  int const size{10};
-  Grid grid{width, height, size};
-  grid.play();
-}
+// int main() {
+//   int const width{800};
+//   int const height{800};
+//   int const size{40};
 
-// compile --> g++ gameOfLife.cpp -lsfml-graphics -lsfml-window -lsfml-system
+//   Grid grid{width, height, size};
+//   grid.data[83].setAlive(true);
+//   grid.data[84].setAlive(true);
+//   grid.data[85].setAlive(true);
+//   grid.play();
+// }
+
+TEST_CASE("Cell::update testing") {
+  int const width{800};
+  int const height{800};
+  int const size{40};
+
+  Grid grid{width, height, size};
+  grid.data[83].setAlive(true);
+  grid.data[84].setAlive(true);
+  grid.data[85].setAlive(true);
+
+  CHECK(grid.data[83].update(grid.data, 4, 4, 20) == false);
+  CHECK(grid.data[83].update(grid.data, 4, 4, 20) == false);
+  CHECK(grid.data[83].update(grid.data, 4, 4, 20) == false);
+}
